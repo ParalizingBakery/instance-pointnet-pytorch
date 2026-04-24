75598421079aaa432eebd84e3bebccea26f77e0d
Wuttiphat Kiddee <95917139+ParalizingBakery@users.noreply.github.com>
2026-04-24 04:06:35 +0000

    theoretically working wps sample dataloader

--- data_utils/WPSSampleDataLoader.py
+++ data_utils/WPSSampleDataLoader.py
@@ -16,6 +16,8 @@ class ScannetDatasetWholeScene:
         stride=0.5,
         block_size=1.0,
         padding=0.001,
+        struct_coord_min=None,
+        struct_coord_max=None,
     ):
         self.block_points = block_points
         self.block_size = block_size
@@ -29,15 +31,6 @@ class ScannetDatasetWholeScene:
 
         self.file_list = [d for d in os.listdir(root)]
 
-        # if self.split == "train":
-        #     self.file_list = [
-        #         d for d in os.listdir(root) if d.find("Area_%d" % test_area) is -1
-        #     ]
-        # else:
-        #     self.file_list = [
-        #         d for d in os.listdir(root) if d.find("Area_%d" % test_area) is not -1
-        #     ]
-
         self.scene_points_list = []
         self.semantic_labels_list = []
         self.room_coord_min, self.room_coord_max = [], []
@@ -46,11 +39,15 @@ class ScannetDatasetWholeScene:
             points = data[:, :3]
             self.scene_points_list.append(data[:, :6])
             self.semantic_labels_list.append(data[:, 6])
-            coord_min, coord_max = (
-                np.amin(points, axis=0)[:3],
-                np.amax(points, axis=0)[:3],
+
+            if (struct_coord_min is None) or (struct_coord_max is None):
+                struct_coord_min, struct_coord_max = (
+                    np.amin(points, axis=0)[:3],
+                    np.amax(points, axis=0)[:3],
+                )
+            self.room_coord_min.append(struct_coord_min), self.room_coord_max.append(
+                struct_coord_max
             )
-            self.room_coord_min.append(coord_min), self.room_coord_max.append(coord_max)
         assert len(self.scene_points_list) == len(self.semantic_labels_list)
 
         # From my look at the add_vote() in test_semseg.py, label weights
@@ -62,76 +59,67 @@ class ScannetDatasetWholeScene:
 
     def __getitem__(self, index):
         point_set_ini = self.scene_points_list[index]
+        assert isinstance(point_set_ini, np.ndarray)
         points = point_set_ini[:, :6]
         labels = self.semantic_labels_list[index]
-        # Looks redundant, self.room_coord_min
-        coord_min, coord_max = np.amin(points, axis=0)[:3], np.amax(points, axis=0)[:3]
-        # From collector, min coordinate is already is 0.0
-        grid_x = int(
-            np.ceil(float(coord_max[0] - coord_min[0] - self.block_size) / self.stride)
-            + 1
-        )
-        grid_y = int(
-            np.ceil(float(coord_max[1] - coord_min[1] - self.block_size) / self.stride)
-            + 1
-        )
+        # Minimum and Maximum of the structure
+        coord_min, coord_max = self.room_coord_min[index], self.room_coord_max[index]
         data_room, label_room, sample_weight, index_room = (
             np.array([]),
             np.array([]),
             np.array([]),
             np.array([]),
         )
-        for index_y in range(0, grid_y):
-            for index_x in range(0, grid_x):
-                s_x = coord_min[0] + index_x * self.stride
-                e_x = min(s_x + self.block_size, coord_max[0])
-                s_x = e_x - self.block_size
-                s_y = coord_min[1] + index_y * self.stride
-                e_y = min(s_y + self.block_size, coord_max[1])
-                s_y = e_y - self.block_size
-                point_idxs = np.where(
-                    (points[:, 0] >= s_x - self.padding)
-                    & (points[:, 0] <= e_x + self.padding)
-                    & (points[:, 1] >= s_y - self.padding)
-                    & (points[:, 1] <= e_y + self.padding)
-                )[0]
-                if point_idxs.size == 0:
-                    continue
-                num_batch = int(np.ceil(point_idxs.size / self.block_points))
-                point_size = int(num_batch * self.block_points)
-                # duplicate points to a multiple of self.block_points
-                replace = (
-                    False if (point_size - point_idxs.size <= point_idxs.size) else True
-                )
-                point_idxs_repeat = np.random.choice(
-                    point_idxs, point_size - point_idxs.size, replace=replace
-                )
-                point_idxs = np.concatenate((point_idxs, point_idxs_repeat))
-                np.random.shuffle(point_idxs)
-                data_batch = points[point_idxs, :]
-                normlized_xyz = np.zeros((point_size, 3))
-                normlized_xyz[:, 0] = data_batch[:, 0] / coord_max[0]
-                normlized_xyz[:, 1] = data_batch[:, 1] / coord_max[1]
-                normlized_xyz[:, 2] = data_batch[:, 2] / coord_max[2]
-                data_batch[:, 0] = data_batch[:, 0] - (s_x + self.block_size / 2.0)
-                data_batch[:, 1] = data_batch[:, 1] - (s_y + self.block_size / 2.0)
-                data_batch[:, 3:6] /= 255.0
-                data_batch = np.concatenate((data_batch, normlized_xyz), axis=1)
-                label_batch = labels[point_idxs].astype(int)
 
-                data_room = (
-                    np.vstack([data_room, data_batch]) if data_room.size else data_batch
-                )
-                label_room = (
-                    np.hstack([label_room, label_batch])
-                    if label_room.size
-                    else label_batch
-                )
-                index_room = (
-                    np.hstack([index_room, point_idxs])
-                    if index_room.size
-                    else point_idxs
-                )
+        sample_min = np.amin(points[:, :3], axis=0)
+        point_idxs = np.arange(0, points.shape[0])
+        
+        if point_idxs.size == 0:
+            raise Exception("No points for sample")
+        num_batch = int(np.ceil(point_idxs.size / self.block_points))
+        point_size = int(num_batch * self.block_points)
+        
+        # duplicate points to a multiple of self.block_points
+        replace = (
+            False if (point_size - point_idxs.size <= point_idxs.size) else True
+        )
+        point_idxs_repeat = np.random.choice(
+            point_idxs, point_size - point_idxs.size, replace=replace
+        )
+        point_idxs = np.concatenate((point_idxs, point_idxs_repeat))
+        np.random.shuffle(point_idxs)
+        data_batch = points[point_idxs, :]
+        normlized_xyz = np.zeros((point_size, 3))
+
+        # For XY, normalize within structure
+        normlized_xyz[:, 0] = (data_batch[:, 0] - coord_min[0]) / (coord_max[0] - coord_min[0])
+        normlized_xyz[:, 1] = (data_batch[:, 1] - coord_min[1]) / (coord_max[1] - coord_min[1])
+
+        # For Z axis, normalize within sample
+        normlized_xyz[:, 2] = (data_batch[:, 2] - sample_min[2]) / np.max(points[:, 2])
+
+        # normalize sample xy to [-0.5, 0.5] when bs is 1.0
+        data_batch[:, 0] = data_batch[:, 0] - (sample_min[0] + (self.block_size / 2.0))
+        data_batch[:, 1] = data_batch[:, 1] - (sample_min[1] + (self.block_size / 2.0))
+        data_batch[:, 3:6] /= 255.0
+        data_batch = np.concatenate((data_batch, normlized_xyz), axis=1)
+        label_batch = labels[point_idxs].astype(int)
+
+        # Possible optimization: stacking copies both array to new index
+        # Keeping them as a list then stacking once should be faster
+        data_room = (
+            np.vstack([data_room, data_batch]) if data_room.size else data_batch
+        )
+        label_room = (
+            np.hstack([label_room, label_batch])
+            if label_room.size
+            else label_batch
+        )
+        index_room = (
+            np.hstack([index_room, point_idxs])
+            if index_room.size
+            else point_idxs
+        )
         data_room = data_room.reshape((-1, self.block_points, data_room.shape[1]))
         label_room = label_room.reshape((-1, self.block_points))
         sample_weight = sample_weight.reshape((-1, self.block_points))
